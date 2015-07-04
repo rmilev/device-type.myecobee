7bfd8cc9206d18aa4693fd68e85153603e5ea6ee
Yves Racine <yracine66@gmail.com>
2015-07-04 17:36:56 -0400

    Minor code formatting

--- smartapps/ScheduleTstatMultiZonesSetup.groovy
+++ smartapps/ScheduleTstatMultiZonesSetup.groovy
@@ -9,7 +9,7 @@
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  Unless required by applicable law or	 agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
@@ -1378,7 +1378,7 @@ private def adjust_vent_settings_in_zone(indiceSchedule) {
 		desiredTemp = thermostat.currentHeatingSetpoint.toFloat().round(1)
 	} else if (mode=='cool') {    
 		desiredTemp = thermostat.currentCoolingSetpoint.toFloat().round(1)
-    } else {
+	} else {
 		desiredTemp = thermostat.currentThermostatSetpoint.toFloat().round(1)
 	}    
 	log.debug("adjust_vent_settings_in_zone>schedule ${scheduleName}, desiredTemp=${desiredTemp}")
