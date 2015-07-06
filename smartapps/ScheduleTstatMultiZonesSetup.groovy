55a9a90d64b873775178c881af95c225785ed5c5
Yves Racine <yracine66@gmail.com>
2015-07-06 19:44:10 -0400

    Version 0.9.1 Set the indiceSchedule to 1 by default in schedulesSetup()

--- smartapps/ScheduleTstatMultiZonesSetup.groovy
+++ smartapps/ScheduleTstatMultiZonesSetup.groovy
@@ -43,7 +43,7 @@ def generalSetupPage() {
 	dynamicPage(name: "generalSetupPage", uninstall: true, nextPage: roomsSetupPage) {
 		section("About") {
 			paragraph "ScheduleTstatZones, the smartapp that enables Heating/Cooling zoned settings at selected thermostat(s) coupled with z-wave vents (optional) for better temp settings control throughout your home"
-			paragraph "Version 0.9\n\n" +
+			paragraph "Version 0.9.1\n\n" +
 				"If you like this app, please support the developer via PayPal:\n\nyracine@yahoo.com\n\n" +
 				"Copyright©2015 Yves Racine"
 			href url: "http://github.com/yracine", style: "embedded", required: false, title: "More information...",
@@ -407,7 +407,7 @@ def schedulesSetup(params) {
 	location.modes.each {
 		enumModes << it.name
 	}    
-	def indiceSchedule=0
+	def indiceSchedule=1
 	// Assign params to indiceSchedule.  Sometimes parameters are double nested.
 	if (params?.indiceSchedule || params?.params?.indiceSchedule) {
 
