f93a669d16e7b5cc6f58d8ee6e126a5cedfb2f33
Yves Racine <yracine66@gmail.com>
2015-07-14 17:38:38 -0400

    V1.9.7 Added error check after poll()

--- smartapps/MyEcobeeInit.groovy
+++ smartapps/MyEcobeeInit.groovy
@@ -43,7 +43,7 @@ def about() {
  	dynamicPage(name: "about", install: false, uninstall: true) {
  		section("About") {	
 			paragraph "My Ecobee Init, the smartapp that connects your Ecobee thermostat to SmartThings via cloud-to-cloud integration"
-			paragraph "Version 1.9.6\n\n" +
+			paragraph "Version 1.9.7\n\n" +
 			"If you like this app, please support the developer via PayPal:\n\nyracine@yahoo.com\n\n" +
 			"Copyright©2014 Yves Racine"
 			href url:"http://github.com/yracine/device-type.myecobee", style:"embedded", required:false, title:"More information...", 
@@ -423,10 +423,11 @@ def takeAction() {
 		try {        
 			d.poll()
 			def exceptionCheck = d.currentVerboseTrace
-			if ((exceptionCheck.contains("exception") && (!exceptionCheck.contains("Java.util.concurrent.TimeoutException")))) {  
-			// check if there is any exception reported in the verboseTrace associated to the device (except the ones linked to rate limiting).
+			if ((exceptionCheck.contains("exception") || (exceptionCheck.contains("error")) && 
+            	(!exceptionCheck.contains("Java.util.concurrent.TimeoutException")))) {  
+			// check if there is any exception or an error reported in the verboseTrace associated to the device (except the ones linked to rate limiting).
 				state.exceptionCount=state.exceptionCount+1    
-				log.error "found exception after polling, exceptionCount= ${state?.exceptionCount}: $exceptionCheck" 
+				log.error "found exception/error after polling, exceptionCount= ${state?.exceptionCount}: $exceptionCheck" 
 			} else {             
 				// reset exception counter            
 				state?.exceptionCount=0       
@@ -439,7 +440,7 @@ def takeAction() {
 	if (state?.exceptionCount>=MAX_EXCEPTION_COUNT) {
 		// need to authenticate again    
 		atomicState.authToken= null                    
-		msg="too many exceptions ({state?.exceptionCount}), need to re-authenticate at ecobee..." 
+		msg="too many exceptions/errors ({state?.exceptionCount}), need to re-authenticate at ecobee..." 
 		send "MyEcobeeInit> ${msg}"
 		log.error msg
 	}    
